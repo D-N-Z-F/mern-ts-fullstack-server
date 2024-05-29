0fa7fb4cad9c35dbd245086266c60b3b46b3d137
Daryl <daryl@globalconnect.com>
2024-05-29 17:45:00 +0800

    2nd commit

--- connection.ts
+++ connection.ts
@@ -2,11 +2,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const { DB_NAME } = process.env;
+const { DB_NAME, DB_HOST, DB_USER, DB_PASSWORD } = process.env;
 
 const connect = async () => {
   try {
-    await mongoose.connect(`mongodb://127.0.0.1:27017/${DB_NAME}`);
+    await mongoose.connect(
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Dee`
+    );
     console.log("Connected to MongoDB");
   } catch (e: unknown) {
     if (e instanceof Error)
